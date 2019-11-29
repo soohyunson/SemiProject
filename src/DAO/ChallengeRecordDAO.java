d48c758fa22e4fec76747f43591c2c251e07b6f7
kks <kks@kks.com>
2019-11-29 17:48:34 +0900

    board delete complete

--- src/DAO/ChallengeRecordDAO.java
+++ src/DAO/ChallengeRecordDAO.java
@@ -63,6 +63,19 @@ public class ChallengeRecordDAO {
 		
 		}
 	}
+	public int deleteByChall(int challenge_numm) throws SQLException, Exception {
+		String sql="delete from challenge_record where challenge_numm =?";
+		try(Connection conn = getConnection();
+				PreparedStatement pstat = conn.prepareStatement(sql);){
+			pstat.setInt(1, challenge_numm);
+			
+			int result = pstat.executeUpdate();
+			conn.commit();
+			
+			return result;
+		
+		}
+	}
 	
 	
 	// ������ ���� (ç���� �Խ��� seq�� �Ѱ������)
@@ -137,6 +150,30 @@ public class ChallengeRecordDAO {
 		try(Connection conn = getConnection();
 				PreparedStatement pstat = conn.prepareStatement(sql);){
 				pstat.setString(1, id);
+				try(ResultSet rs = pstat.executeQuery()){
+			
+			ArrayList<Challenge_recordDTO> list = new ArrayList<>();
+			while(rs.next()) {
+				Challenge_recordDTO dto = new Challenge_recordDTO();
+				dto.setSeq(rs.getInt(1));
+				dto.setSuccess(rs.getString(2));
+				dto.setChallenge_num(rs.getInt(3));
+				dto.setMember_id(rs.getString(4));
+				
+				
+				list.add(dto);
+
+			}
+			return list;
+		}
+		}
+
+	}
+	public ArrayList<Challenge_recordDTO> selectAll (int challenge_numm) throws Exception{
+		String sql ="select * from challenge_record where challenge_numm=?";
+		try(Connection conn = getConnection();
+				PreparedStatement pstat = conn.prepareStatement(sql);){
+				pstat.setInt(1, challenge_numm);
 				try(ResultSet rs = pstat.executeQuery()){
 			
 			ArrayList<Challenge_recordDTO> list = new ArrayList<>();
